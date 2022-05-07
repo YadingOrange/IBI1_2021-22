fd1b49d92c546f9689bedc92b2955335d132e6d7
YadingOrange <102206115+YadingOrange@users.noreply.github.com>
2022-05-07 15:16:58 +0800

    Update pizza_party.py

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -1,10 +1,17 @@
-#n represents a given number of straight cuts.
-#p represents the maximum number of pizza that can be made with n straight cuts.
+# n represents a given number of straight cuts.
+# p represents the maximum number of pizza that can be made with n straight cuts.
+# initialize n and p
+# use a while loop to compute the minimum value of n when p >= 64
+# print the answer
+
+
+
 n = 0
-p = 1
+p = 0
 number = 64
 while p < number:
-    p = (n**2+n+2)/2
     n += 1
-    print("the number of cuts is:",n)
-    
+    p = (n**2+n+2)/2
+
+print("the number of cuts is:", n)
+print("the number of pieces created at n cuts is", p)
