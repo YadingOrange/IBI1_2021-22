8845c4088c2b78c692c1c778f29eb5b165977c3c
YadingOrange <102206115+YadingOrange@users.noreply.github.com>
2022-05-07 14:47:13 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,12 +1,25 @@
-a = 19245301
-b = 4218520
-c = 271
+a = 19245301  # 2022
+b = 4218520  # 2021
+c = 271  # 2020
 d = b-c
 e = a-b
-d<e
-True
+if d < e:
+    print("e is greater")
+if d > e:
+    print("d is greater")
 f = d/c
 g = e/b
-f<g
-False
-The rate of new cases was greater in 2020.
+if f < g:
+    print("The rate of new cases was greater in 2021.")
+if f > g:
+    print("The rate of new cases was greater in 2020.")
+
+
+X = 0
+Y = 1
+bool(X)
+bool(Y)
+print("X:", bool(X))
+print("Y:", bool(Y))
+Z = bool(X) and bool(Y)
+print("X and Y:", Z)
