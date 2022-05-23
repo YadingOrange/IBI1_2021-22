85815fcb93e6098a0503cf67300faa20cdca510a
YadingOrange <2661756161@qq.com>
2022-05-23 21:58:33 +0800

    add fragment_counter.py

--- Practical8/fragment_counter.py
+++ Practical8/fragment_counter.py
@@ -1,8 +1,4 @@
 import re
 seq="ATGCAATCGACTACGATCAATCGAGGGCC"
-number=len(re.findall("GAATTC,seq"))+1
-<<<<<<< HEAD
+number=len(re.findall("GAATTC",seq))+1
 print(number)
-=======
-print(number)
->>>>>>> d5a6fb4142746598b2ab345d6ead21ae34b8831e
